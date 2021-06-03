47eca9e88eaaa69bec5b762ad8a1acadcbd831a7
Rudy Laurent <rudy.laurent@factorfx.com>
2021-06-03 17:48:40 +0200

    Move config files

---
+++ lib/core/config.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:ocs_agent/core/json_utils.dart';
+import 'package:ocs_agent/core/files_utils.dart';
+
+class Config{
+  final String inventoryFilename = "config/inventory.json";
+  final String coreFilename = "config/core.json";
+
+  JsonUtils jsonUtils;
+  FilesUtils filesUtils;
+
+  File inventory;
+  File core;
+
+  Config() {
+    this.jsonUtils = JsonUtils();
+    this.filesUtils = FilesUtils();
+    this.inventory = File(inventoryFilename);
+    this.core = File(coreFilename);
+  }
+
+  String getInventoryConfigs(){
+    return jsonUtils.getContent(inventory).toString();
+  }
+
+  String getInventoryConfig(String key){
+    return jsonUtils.getContentByKey(inventory, key);
+  }
+
+  void updateInventoryConfig(String _index, String _value){
+    String str = jsonUtils.setContent(inventory, _index, _value);
+    filesUtils.rewriteFile(inventory, str);
+  }
+
+  String getCoreConfigs(){
+    return jsonUtils.getContent(core).toString();
+  }
+
+  String getCoreConfig(String key){
+    return jsonUtils.getContentByKey(core, key);
+  }
+
+  void updateCoreConfig(String _index, String _value){
+    String str = jsonUtils.setContent(core, _index, _value);
+    filesUtils.rewriteFile(core, str);
+  }
+
+
+}
