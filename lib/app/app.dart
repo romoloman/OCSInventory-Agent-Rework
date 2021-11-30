1a028a019880d9f4bf7a433704d2463b5893e88a
Djily Sarr <djily.sarr@factorfx.com>
2021-11-30 15:38:30 +0100

    Send the linux base to asset/bases

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:ocs_agent/core/api.dart' as api;
 
-///in this main section we send the body to the asset/bases
+import 'package:ocs_agent/core/inventory/linux/baseLinux.dart' as baseLinux;
+
+///in this main section we send the [body] to the asset/bases
 void main(List<String> args) async {
   var sendBody = new api.Api();
 
@@ -9,4 +11,6 @@ void main(List<String> args) async {
   sendBody.apiCheck();
 
   sendBody.getHeader();
+
+  sendBody.sendInventory(await baseLinux.getBody());
 }
