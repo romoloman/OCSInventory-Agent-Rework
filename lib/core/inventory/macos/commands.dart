7c73d6cb2eeb1d00c0d192a30c95cc859130590b
Gilles DUBOIS <gilles.dubois@factorfx.com>
2021-12-07 14:07:15 +0100

    Apply command exec fix from linux on mac

--- lib/core/inventory/macos/commands.dart
+++ lib/core/inventory/macos/commands.dart
@@ -1,7 +1,19 @@
-//import 'dart:io';
+import 'dart:io';
 
+/// Class for execute command on Mac.
 class MacOSCommand {
-  Future<String> commandCmd(String commandLine) async {
-    return null;
+  /// Execute [commandLine] to Shell.
+  Future<String> commandShell(String commandLine) async {
+    List<String> args = commandLine.split(" ");
+    String command = args[0];
+    args.removeAt(0);
+    if (args == null) {
+      args = [];
+    }
+
+    var process;
+    await Process.run(command, args).then((value) => process = value.stdout);
+
+    return process;
   }
 }
