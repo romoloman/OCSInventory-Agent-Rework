1e32629fbee8eab91ee3489fe0ea931c308f3a86
Djily90 <djilysarr275@gmail.com>
2021-12-10 12:18:57 +0100

    Send the Windows Base to asset/bases

--- lib/core/inventory/windows/baseWindows.dart
+++ lib/core/inventory/windows/baseWindows.dart
@@ -14,10 +14,10 @@ dynamic getBody() async {
   regexMacAddr = RegExp(r"([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})");
 
   /// Get the Mac Address value
-  String getMacAddr;
+  var getMacAddr;
   getMacAddr = regexMacAddr.stringMatch(macAddr);
 
-  /// Command get the domain
+  /// Command get to the domain
   dynamic domain;
   domain = await windowsCommand.commandCmd("ipconfig");
 
@@ -29,13 +29,37 @@ dynamic getBody() async {
   );
 
   /// Get domain value
-  String getDomain;
+  var getDomain;
   getDomain = regexDomain.stringMatch(domain);
+
+  /// Command get to get the IP
+  String ip;
+  ip = await windowsCommand.commandCmd("ipconfig");
+
+  /// RegExp to match IP
+  RegExp regexIP;
+  regexIP = RegExp(r"((?:[0-9]{1,3}\.){3}[0-9]{1,3})");
+
+  /// Get IP value
+  var getIP;
+  getIP = regexIP.stringMatch(ip);
 
   dynamic body = ({
     "name": await windowsCommand.commandCmd("hostname"),
-    "srcmac": getMacAddr,
-    "domain": getDomain,
+    "description": await windowsCommand.commandPowershell(
+        "(Get-WMIObject -Class Win32_ComputerSystemProduct).Description"),
+    "serial": await windowsCommand.commandPowershell(
+        "(Get-WMIObject win32_operatingsystem).SerialNumber"),
+    "osname": (await windowsCommand
+            .commandPowershell("(Get-WMIObject win32_operatingsystem).name"))
+        .split("|")[0],
+    "osversion": await windowsCommand
+        .commandPowershell("(Get-WMIObject win32_operatingsystem).Version"),
+    "uuid": await windowsCommand.commandPowershell(
+        "(Get-WMIObject -Class Win32_ComputerSystemProduct).UUID"),
+    "srcip": await getIP,
+    "srcmac": await getMacAddr,
+    "domain": await getDomain,
   });
 
   return body;
