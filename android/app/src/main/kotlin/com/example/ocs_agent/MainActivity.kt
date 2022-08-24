6ed819088c810879389100fd1ed580b63c8870f7
Gilles Dubois <gilles.dubois@factorfx.com>
2022-08-24 11:21:36 +0200

    Fix local mega linter autofix

--- android/app/src/main/kotlin/com/example/ocs_agent/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ocs_agent/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.ocs_agent
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,19 @@
+// OCSInventory Agent
+// Copyright (C) OCSInventory-NG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import 'dart:io' show Platform;
 
 import 'package:ocs_agent/core/api.dart' as api;
@@ -6,12 +22,11 @@ import 'package:ocs_agent/core/inventory/linux/baseLinux.dart' as baseLinux;
 
 import 'package:ocs_agent/core/inventory/macos/baseMacOS.dart' as baseMacOS;
 
-import 'package:ocs_agent/core/inventory/windows/baseWindows.dart'
-    as baseWindows;
+import 'package:ocs_agent/core/inventory/windows/baseWindows.dart' as baseWindows;
 
 ///in this main section we send the [body] to the asset/bases
 void main(List<String> args) async {
-  var sendBody = new api.Api();
+  var sendBody = api.Api();
 
   sendBody.generateToken();
 
@@ -28,7 +43,7 @@ void main(List<String> args) async {
   } else if (Platform.isWindows) {
     sendBody.sendInventory(await baseWindows.getBody());
   } else {
-    sendBody.logger.error("Error Platform");
+    sendBody.logger.error('Error Platform');
   }
 
   sendBody.getInventory();
