fc0835d7c20004b17618e93e1f5e661387d7826d
Antoine ROBIN <antoine.robin@factorfx.com>
2024-01-11 10:44:49 +0100

    feat(deployment.dart) add deployment class

---
+++ lib/core/deployment.dart
@@ -0,0 +1,71 @@
+// OCSInventory Agent
+// Copyright (C) OCSInventory-NG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import 'dart:ffi';
+
+class Deployment {
+  Map<int, String> errorCodes = new Map();
+
+  Deployment() {
+    errorCodes = {
+      0: "WAITING NOTIFICATION",
+      1: "NOTIFIED",
+      2: "SUCCESS",
+      3: "ERROR_EXIT_CODE_XXX",
+      4: "ERR_ALREADY_SETUP",
+      5: "ERR_BAD_ID",
+      6: "ERR_BAD_DIGEST",
+      7: "ERR_DOWNLOAD_INFO",
+      8: "ERR_DOWNLOAD_PACK",
+      9: "ERR_BUILD",
+      10: "ERR_UNZIP",
+      11: "ERR_OUT_OF_SPACE",
+      12: "ERR_BAD_PARAM",
+      13: "ERR_EXECUTE_PACK",
+      14: "ERR_EXECUTE",
+      15: "ERR_CLEAN",
+      16: "ERR_DONE_FAILED",
+      17: "ERR_TIMEOUT",
+      18: "ERR_ABORTED"
+    };
+  }
+
+  bool checkDownload(int agentID) {
+    return false;
+  }
+
+  Map<int, dynamic> getActions(int agentID) {
+    return new Map();
+  }
+
+  void executeAction(bool containFile, String actionType) {}
+
+  dynamic getFile() {
+    return false;
+  }
+
+  int executeFile(dynamic file) {
+    return 0;
+  }
+
+  int storeFile(dynamic file) {
+    return 0;
+  }
+
+  int launchFile(dynamic file) {
+    return 0;
+  }
+}
