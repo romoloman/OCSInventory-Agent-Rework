0e75f7d5a9d10f7d3e822fe7330faed1ac52561c
Antoine ROBIN <antoine.robin@factorfx.com>
2024-02-21 09:55:47 +0100

    feat(deployment): add check download method and get action method

--- lib/core/deployment.dart
+++ lib/core/deployment.dart
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import 'dart:convert';
+import 'package:sprintf/sprintf.dart';
+
 import 'package:ocs_agent/core/config.dart';
 import 'package:ocs_agent/core/log.dart';
 
@@ -23,9 +26,26 @@ class Deployment {
   late Config config;
   late Logger logger;
 
+  late HTTPUtils httpUtils;
+
+  late var url;
+
+  late List<dynamic> results;
+  late Map<int, dynamic> actions;
+
   Map<int, String> errorCodes = new Map();
 
+  /// Constructor.
   Deployment() {
+    this.config = new Config();
+    this.logger = new Logger();
+
+    this.httpUtils = new HTTPUtils();
+
+    this.url = config.getInventoryConfig("url");
+
+    this.actions = new Map();
+
     errorCodes = {
       0: "WAITING NOTIFICATION",
       1: "NOTIFIED",
@@ -49,15 +69,58 @@ class Deployment {
     };
   }
 
+  /// Check if there is packages to download.
   Future<bool> checkDownload(int assetID) async {
-    return false;
+    logger.info("Enabling deployment module...");
+    var response = await httpUtils.get(
+        "checkDownload",
+        Uri.parse(url + "/deployment/results/?asset=$assetID&status=1"),
+        httpUtils.getHeader(config));
+    logger.verbose(response["message"]);
+    if (response["status_code"] == 200 &&
+        jsonDecode(response["body"]).isNotEmpty) {
+      logger.info("Assigned packages found!");
+      results = jsonDecode(response["body"]);
+      return true;
+    } else {
+      logger.info("Any package has been found on the server.");
+      return false;
+    }
   }
 
-  Map<int, dynamic> getActions(int assetID) {
-    return new Map();
+  /// Get actions from assigned packages.
+  Future<bool> getActions(int assetID) async {
+    logger.info("Collecting actions...");
+    for (var element in results) {
+      try {
+        var response = await httpUtils.get(
+            "getActions",
+            Uri.parse(url +
+                "/deployment/actions/?package=" +
+                element["package"].toString()),
+            httpUtils.getHeader(config));
+        logger.verbose(response["message"]);
+        if (response["status_code"] == 200) {
+          actions.putIfAbsent(element["package"], () => response["body"]);
+        } else {
+          logger.error("Can't collect actions from the server.");
+          return false;
+        }
+      } catch (exception) {
+        logger.error(sprintf("HTTP query: %s", [exception.toString().trim()]));
+        logger.serverLogger(assetID, 5,
+            sprintf("HTTP query: %s", [exception.toString().trim()]));
+        return false;
+      }
+    }
+    logger.info("Actions collected");
+    return true;
   }
 
-  void executeAction(bool containFile, String actionType) {}
+  /// Execute actions from assigned packages.
+  void executeActions() {
+    logger.info("Executing actions...");
+  }
 
   dynamic getFile() {
     return false;
