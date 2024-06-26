a00421d46d160747c086c3b7df100bdde208d794
Djily SARR <djilysarr275@gmail.com>
2024-06-26 12:32:06 +0200

    feat(installer): App agent ocs xcode

---
+++ lib/setup/macos/ocsng_app-xcode/ocscontact.h
@@ -0,0 +1,20 @@
+//
+// OCSINVENTORY-NG 
+// Copyleft Guillaume PROTET 2011
+// Web : http://www.ocsinventory-ng.org
+//
+// This code is open source and may be copied and modified as long as the source
+// code is always made freely available.
+// Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
+//
+//
+
+#ifdef __OBJC__
+    #import <Cocoa/Cocoa.h>
+#endif
+
+@interface ocscontact : NSObject {
+
+}
+
+@end
