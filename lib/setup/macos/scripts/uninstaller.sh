d23033351e67b257e8e71f9469e23592120ed7ef
Djily SARR <djilysarr275@gmail.com>
2024-06-26 12:33:45 +0200

    feat(installer): script to uninstall the agent

---
+++ lib/setup/macos/scripts/uninstaller.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OSVER=`uname -r`
+echo "OSVer is $OSVER"
+
+PID=`ps ax -e | grep OCS-NG | grep -v grep | grep -v $0 | awk '{print $1}'`
+if [ "$PID" !=  "" ]; then
+	echo "killing process: $PID"
+	sudo kill $PID
+fi
+
+FILES="/Library/Receipts/OCS-NG* /etc/ocsinventory-agent/ /var/lib/ocsinventory-agent/ /Applications/OCS-NG.app /var/log/ocsinventory-agent/"
+
+if [ "$OSVER" == "7.9.0" ]; then
+	FILES="$FILES /Library/StartupItems/OCSInventory"
+else
+	FILES="$FILES /Library/LaunchDaemons/org.ocsinventory.agent.plist"
+
+	echo 'Stopping and unloading service'
+	sudo launchctl stop org.ocsinventory.agent
+	sudo launchctl unload /Library/LaunchDaemons/org.ocsinventory.agent.plist
+	sudo rm /Library/LaunchDaemons/org.ocsinventory.agent.plist
+fi
+
+for FILE in $FILES; do
+  echo 'removing '.$FILE
+  rm -f -R $FILE
+done
+
