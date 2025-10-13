6b15a6f69937916ba8076e23d6a9010a85bf1fb8
Lea9250 <lea.droguet@factorfx.com>
2025-10-13 10:57:03 +0200

    fix(import): fix agent not lauching bc of missing imports

--- lib/core/inventory/linux/baseLinux.dart
+++ lib/core/inventory/linux/baseLinux.dart
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // External package imports
+import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:sprintf/sprintf.dart';
