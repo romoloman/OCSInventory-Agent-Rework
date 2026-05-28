7bd0bfa704b9baf5d4f4bdf8216a4ee1fbe26fe9
hugotheault <hugo.theault.1@gmail.com>
2026-05-28 11:39:01 +0200

    fix(deployment): remove local httpclient declaration

--- lib/core/deployment.dart
+++ lib/core/deployment.dart
@@ -19,6 +19,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:archive/archive_io.dart';
+import 'package:http/http.dart' as http;
 import 'package:sprintf/sprintf.dart';
 
 // Core imports
@@ -657,7 +658,6 @@ class Deployment {
     }
 
     Map<String, dynamic> result = {"status": false, "error": ""};
-    late HttpClient client;
     int retryCounter = 0;
 
     Future<String> completerValue = Future.value("");
@@ -686,17 +686,15 @@ class Deployment {
       bool responseStreamStatus = false;
       Completer<String> completer = Completer<String>();
       try {
-        client = HttpClient();
         result["status"] = false;
         result["error"] = "";
 
-        client.getUrl(Uri.parse(fileUrl)).then((HttpClientRequest request) {
-          return request.close();
-        }).then((HttpClientResponse response) {
+        final request = http.Request("GET", Uri.parse(fileUrl));
+        httpUtils.ioClient.send(request).then((response) {
           if (response.statusCode == HttpStatus.ok) {
             logger.debug(this.runtimeType.toString(),
                 "Successfully downloaded file: $fileUrl");
-            response.pipe(fileSaveLocal.openWrite()).then((fileAdded) async {
+            response.stream.pipe(fileSaveLocal.openWrite()).then((fileAdded) async {
               // Save the file directly if not zipped
 
               if (fileAdded.existsSync()) {
@@ -881,18 +879,17 @@ class Deployment {
               logger.error(this.runtimeType.toString(),
                   "File save failed on retry #${retryCounter}: ${result["error"]}");
               responseStreamStatus = false;
-              return response.drain();
+              return response.stream.drain();
             } else {
               logger.error(this.runtimeType.toString(), result["error"]);
               responseStreamStatus = false;
-              return response.drain();
+              return response.stream.drain();
             }
           }
         });
       } finally {
         // Get the future from the completer
         completerValue = completer.future;
-        client.close();
       }
 
       await completerValue.then((value) async {
